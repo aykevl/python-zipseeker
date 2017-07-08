2390b1f6786cf1c8c18544510171710dd66409d6
Ayke van Laethem <aykevanlaethem@gmail.com>
2017-07-08 23:25:33 +0200

    Some more PyPI fixes

--- setup.py
+++ setup.py
@@ -2,13 +2,16 @@
 
 from distutils.core import setup
 
+VERSION='1.0.7'
+
 setup(name='zipseeker',
       packages=['zipseeker'],
-      version='1.0.1',
+      version=VERSION,
       description='Create a streamable and (somewhat) seekable .ZIP file',
+      long_description=open('README.rst').read(),
       author='Ayke van Laethem',
       author_email='aykevanlaethem@gmail.com',
       url='https://github.com/aykevl/zipseeker',
       keywords=['zip', 'http', 'streaming'],
-      download='https://github.com/peterldowns/python-zipseeker/archive/1.0.1.tar.gz'
+      download_url='https://github.com/aykevl/python-zipseeker/archive/v%s.zip' % VERSION,
      )
